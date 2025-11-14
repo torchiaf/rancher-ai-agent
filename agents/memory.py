c0530042144cae9ab6581cd2aa0126b919d20aee
Francesco Torchia <francesco.torchia@suse.com>
2025-11-14 18:22:20 +0100

    [hw] Add mysql client; check session permissions
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- agents/memory.py
+++ agents/memory.py
@@ -1,21 +1,20 @@
-from clients import RedisClient
+from clients import RedisClient, MySQLClient
 
 """
-MemoryAgent provides an interface for storing and retrieving
-conversation history using a cache.
-
-In the future, this can be extended to support database storage as well by implementing a database client.
-
-The database client can be added as an additional parameter to the MemoryAgent constructor,
-and the store and fetch methods can be updated to interact with both the cache and the database as needed.
-
-Or,
-
-it can be designed as a separate DB pod that subscribes to the redis stream and stores the messages in the database.
+MemoryAgent
+ - Provides an interface for storing and retrieving conversation history using a cache.
+ - Provides a DB interface for session information retrieval.
 """
 class MemoryAgent:
-    def __init__(self, cache_client_url: str):
+    def __init__(self, cache_client_url: str, db_host: str, db_port: int, db_user: str, db_password: str, db_database: str):
         self.cache_client = RedisClient(cache_client_url)
+        self.db_client = MySQLClient(
+            host=db_host,
+            port=db_port,
+            user=db_user,
+            password=db_password,
+            database=db_database
+        )
 
     async def destroy(self):
         await self.cache_client.disconnect()
@@ -31,15 +30,39 @@ class MemoryAgent:
     async def fetch_messages(self, session_id: str, max_count: int = 10, role_filter: list[str] | None = None) -> list[str]:
         return await self.cache_client.fetch_messages(session_id, max_count, role_filter)
 
-async def create_memory_agent(cache_client_url: str) -> MemoryAgent:
+    async def get_session_info(self, session_id: str, user_id: str) -> dict | None:
+        return await self.db_client.get_session_info(session_id, user_id)
+
+    async def check_session_permissions(self, session_id: str, user_id: str) -> bool:
+        res = await self.get_session_info(session_id, user_id)
+        if res:
+            return True
+        return False
+
+async def create_memory_agent(
+        cache_client_url: str,
+        db_host: str,
+        db_port: int,
+        db_user: str,
+        db_password: str,
+        db_database: str
+    ) -> MemoryAgent:
     """
     Creates a MemoryAgent instance and connects it to the cache and database.
 
     Returns:
         A connected MemoryAgent instance.
     """
-    agent = MemoryAgent(cache_client_url)
+    agent = MemoryAgent(
+        cache_client_url=cache_client_url,
+        db_host=db_host,
+        db_port=db_port,
+        db_user=db_user,
+        db_password=db_password,
+        db_database=db_database
+    )
 
     await agent.cache_client.connect()
+    await agent.db_client.check_connection()
 
     return agent

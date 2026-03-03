d11649c0b2d98c196956c6980ad94ab9bcfca2eb
Francesco Torchia <francesco.torchia@suse.com>
2026-03-03 17:59:02 +0100

    temp
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- app/routers/websocket.py
+++ app/routers/websocket.py
@@ -116,7 +116,7 @@ async def websocket_endpoint(websocket: WebSocket, thread_id: str = None, llm: B
         except Exception as e:
             logging.error(f"An error occurred: {e}", exc_info=True)
             if websocket.client_state == WebSocketState.CONNECTED:
-                await websocket.send_text(f'<error>{{"message": "{str(e)}"}}</error>')
+                await websocket.send_text(f'<error>{json.dumps({"message": str(e)})}</error>')
             else:
                 break
         finally:
